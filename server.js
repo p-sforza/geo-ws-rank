3e742378a838e1d1e0ff957ee9674c7fd40a0f7f
psforza <psforza@redhat.com>
2016-10-21 01:14:24 +0200

    client implementation

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var WebSocketServer = require('websocket').server;
+var WebSocketClient = require('websocket').client;
+
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
@@ -64,3 +66,45 @@ wsServer.on('request', function(request) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
+
+// CLient implementation
+var socket;
+var connectionRequest;
+var url = "ws://websocket.cloudapps.novalocal:80/ws/";
+
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+ 
+client.on('connect', function(connection) {
+    console.log('WebSocket Client Connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+    
+    function sendNumber() {
+        if (connection.connected) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            connection.sendUTF(number.toString());
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    //sendNumber();
+});
+ 
+client.connect(url, 'echo-protocol');
+
+
+	
+	
+	
